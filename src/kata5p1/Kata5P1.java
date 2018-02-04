cbff33c15d74fd97154cef5c1dabbdfd604e280b
Alba Alonso Santana <Saturno@Saturno>
2018-02-04 17:25:28 +0000

    kata5P1: segundo

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -3,7 +3,6 @@ package kata5p1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -12,15 +11,7 @@ public class Kata5P1 {
         Class.forName("org.sqlite.JDBC");
         Connection connec = DriverManager.getConnection("jdbc:sqlite:Kata5.db");	
         Statement st = connec.createStatement();	
-        String	query = "SELECT * FROM PEOPLE;";	
-        ResultSet rs = st.executeQuery(query);	
-        System.out.println("ID | Nombre | Apellido | Departamento ");
-        while(rs.next()){	
-            Integer id	= rs.getInt(1);	
-            String name	= rs.getString(2);	
-            String apellido = rs.getString(3);	
-            String departamento	= rs.getString(4);	
-            System.out.println(id + "  | "+ name + " | " + apellido+ " | " + departamento);
-	}	
+        String	query = "CREATE	TABLE IF NOT EXISTS MAIL ('Id'	INTEGER	PRIMARY	KEY AUTOINCREMENT,'Mail' TEXT NOT NULL);";		
+        st.execute(query);	  	
     }   
 }
